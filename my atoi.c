1200b35365b948aebe0de9ac844b5ce438acd09d
Fud0o0 <lukas.vaquette@oteria.fr>
2026-03-09 14:52:58 +0100

    Add HTML files for the exercise on March 9, including head and title macros

--- my atoi.c
+++ my atoi.c
@@ -3,7 +3,7 @@ int my_atoi(const char *s) {
     int sign = 1;
     int i = 0;
 
-    // 1. Check for sign
+    
     if (s[i] == '-') {
         sign = -1;
         i++;
@@ -11,13 +11,13 @@ int my_atoi(const char *s) {
         i++;
     }
 
-    // 2. Process digits only
+    
     for (; s[i] != '\0'; i++) {
         if (s[i] >= '0' && s[i] <= '9') {
             res = res * 10 + (s[i] - '0');
         }
     }
 
-    // 3. Apply the sign
+    
     return res * sign;
 }
